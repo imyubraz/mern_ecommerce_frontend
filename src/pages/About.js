42a230f22ed8e5a7683949d4013787e36a4c7d94
Yubraj <imyubraz@gmail.com>
2023-04-21 22:20:41 +0545

    Contact, About and Policy pages added.

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,27 @@ import Layout from '../components/layout/Layout'
 
 const About = () => {
     return (
-        <Layout>About Page</Layout>
+        <Layout>
+            <div className="row contact">
+                <div className="col-md-6 ">
+                    <img
+                        src="/images/about.jpeg"
+                        alt="contactus"
+                        style={{ width: "100%" }}
+                    />
+                </div>
+                <div className="col-md-4">
+                    <p className="text-justify mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                        officiis obcaecati esse tempore unde ratione, eveniet mollitia,
+                        perferendis eius temporibus dicta blanditiis doloremque explicabo
+                        quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
+                        accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
+                        commodi illum quidem neque tempora nam.
+                    </p>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -3,7 +3,35 @@ import Layout from '../components/layout/Layout'
 
 const Policy = () => {
     return (
-        <Layout>Policy Page</Layout>
+        <Layout>
+            <div className="row contact">
+                <div className="col-md-6 ">
+                    <img src="/images/contactus.jpeg" alt="contactus" style={{ width: "100%" }} />
+                </div>
+                <div className="col-md-4">
+                    <ul>
+                        <li>
+                            <p>Policy 1 : Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa debitis quam labore?
+                            </p>
+                        </li>
+                        <li>
+                            <p>Policy 2 : Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eos nulla neque
+                                labore consectetur harum.</p>
+                        </li>
+                        <li>
+                            <p>Policy 3 : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, fugit.</p>
+                        </li>
+                        <li>
+                            <p>Policy 4: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente culpa
+                                illum?</p>
+                        </li>
+                        <li>
+                            <p>Policy 5 : Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
