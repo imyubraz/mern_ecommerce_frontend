b24eb2138db8981484808cd0c598072899660052
Yubraj <imyubraz@gmail.com>
2023-04-21 14:34:17 +0545

    Basic routing setup done with react-router-dom v6.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import NotFoundPage from './pages/NotFoundPage';
+
 function App() {
   return (
     <>
-      <Layout>
-        <h1>E-Commerce App</h1>
-      </Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
