af9035681b051e90e41f1ee710b957049aac43e8
Yubraj <iyubrajpoudel@gmail.com>
2023-04-22 10:47:50 +0545

    Form default submit beahaviour prevented, axios &  react-toastify packages installed, and react-toastify is used to show toast messages on form submission

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,25 +1,41 @@
 import React, { useState } from 'react'
 import Layout from '../../components/layout/Layout'
 
+// Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleSubmit = (e) => {
+        // preventing default submit behaviour
+        e.preventDefault();
+        console.log(email, password);
+
+        // showing toast messages
+        // toast("Form submitted!");
+        toast.success("Form submitted!");
+    }
+
     return (
         <Layout title="Login | Ecommerce App">
             <>
                 <div className="container d-flex justify-content-center align-items-center vh-100 flex-column">
+                    <ToastContainer position="bottom-right" />
+                    {/*  tweaking position from Toast container (default position => top-right) */}
                     <div className="form-wrapper w-50 shadow-lg p-4">
                         <h2 className='text-center py-2 mb-4'>Login</h2>
-                        <form >
+                        <form onSubmit={handleSubmit} >
                             <div className="mb-3">
                                 <label htmlFor="emailInput" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="emailInput" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="email" className="form-control" id="emailInput" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="passwordInput" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="passwordInput" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" className="form-control" id="passwordInput" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} required />
                             </div>
                             <button type="submit" className="btn btn-primary px-4">Login</button>
                         </form>
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import Layout from '../../components/layout/Layout'
 
+// Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Register = () => {
 
     // states
@@ -12,37 +16,52 @@ const Register = () => {
     const [address, setAddress] = useState("");
 
 
+    const handleSubmit = (e) => {
+        // preventing default submit behaviour
+        e.preventDefault();
+        console.log(name, username, email, phone, password, address);
+
+        // showing toast messages
+        // toast("Form submitted!");
+        toast.success("Form submitted!", {
+            position: "top-center"
+        });
+    }
+
+
+
     return (
         <Layout title="Register | Ecommerce App">
             <>
                 <div className="container d-flex justify-content-center align-items-center vh-100 flex-column">
+                    <ToastContainer />
                     <div className="form-wrapper w-50 shadow-lg p-4">
                         <h2 className='text-center py-2 mb-4'>Register</h2>
-                        <form >
+                        <form onSubmit={handleSubmit}>
                             <div className="mb-3">
                                 <label htmlFor="nameInput" className="form-label">Name</label>
-                                <input type="text" className="form-control" id="nameInput" placeholder='John Doe' value={name} onChange={(e) => setName(e.target.value)} />
+                                <input type="text" className="form-control" id="nameInput" placeholder='John Doe' value={name} onChange={(e) => setName(e.target.value)} required />
                                 <div id="emailHelp" className=" d-none">We'll never share your email with anyone else.</div>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="usernameInput" className="form-label">Username</label>
-                                <input type="text" className="form-control" id="usernameInput" placeholder='johndoe2023' value={username} onChange={(e) => setUsername(e.target.value)} />
+                                <input type="text" className="form-control" id="usernameInput" placeholder='johndoe2023' value={username} onChange={(e) => setUsername(e.target.value)} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="emailInput" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="emailInput" placeholder='josh@gmail.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <input type="email" className="form-control" id="emailInput" placeholder='josh@gmail.com' value={email} onChange={(e) => setEmail(e.target.value)} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="passwordInput" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="passwordInput" placeholder='Enter strong password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" className="form-control" id="passwordInput" placeholder='Enter strong password' value={password} onChange={(e) => setPassword(e.target.value)} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="phoneInput" className="form-label">Phone</label>
-                                <input type="phone" className="form-control" id="phoneInput" placeholder='987654321' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                                <input type="phone" className="form-control" id="phoneInput" placeholder='987654321' value={phone} onChange={(e) => setPhone(e.target.value)} required />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="addressInput" className="form-label">Address</label>
-                                <input type="address" className="form-control" id="addressInput" placeholder='Kathmandu, Nepal' value={address} onChange={(e) => setAddress(e.target.value)} />
+                                <input type="address" className="form-control" id="addressInput" placeholder='Kathmandu, Nepal' value={address} onChange={(e) => setAddress(e.target.value)} required />
                             </div>
                             <button type="submit" className="btn btn-primary px-4">Register</button>
                         </form>
