53e602ff48952e0ad03530a13f2306806b725361
Yubraj <iyubrajpoudel@gmail.com>
2023-04-22 09:46:38 +0545

    Register and Login page designed.

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,7 +4,25 @@ import Layout from '../../components/layout/Layout'
 const Login = () => {
     return (
         <Layout title="Login | Ecommerce App">
-            <h2>Login Page</h2>
+            <>
+                <div className="container d-flex justify-content-center align-items-center vh-100 flex-column">
+                    <div className="form-wrapper w-50 shadow-lg p-4">
+                        <h2 className='text-center py-2 mb-4'>Login</h2>
+                        <form >
+                            <div className="mb-3">
+                                <label htmlFor="emailInput" className="form-label">Email address</label>
+                                <input type="email" className="form-control" id="emailInput" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="passwordInput" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="passwordInput" />
+                            </div>
+                            <button type="submit" className="btn btn-primary px-4">Login</button>
+                        </form>
+                    </div>
+
+                </div>
+            </>
         </Layout>
     )
 }
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -4,7 +4,42 @@ import Layout from '../../components/layout/Layout'
 const Register = () => {
     return (
         <Layout title="Register | Ecommerce App">
-            <h2>Register Page</h2>
+            <>
+                <div className="container d-flex justify-content-center align-items-center vh-100 flex-column">
+                    <div className="form-wrapper w-50 shadow-lg p-4">
+                        <h2 className='text-center py-2 mb-4'>Register</h2>
+                        <form >
+                            <div className="mb-3">
+                                <label htmlFor="nameInput" className="form-label">Name</label>
+                                <input type="text" className="form-control" id="nameInput" />
+                                <div id="emailHelp" className=" d-none">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="usernameInput" className="form-label">Username</label>
+                                <input type="text" className="form-control" id="usernameInput" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="emailInput" className="form-label">Email address</label>
+                                <input type="email" className="form-control" id="emailInput" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="phoneInput" className="form-label">Phone</label>
+                                <input type="phone" className="form-control" id="phoneInput" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="addressInput" className="form-label">Address</label>
+                                <input type="address" className="form-control" id="addressInput" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="passwordInput" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="passwordInput" />
+                            </div>
+                            <button type="submit" className="btn btn-primary px-4">Register</button>
+                        </form>
+                    </div>
+
+                </div>
+            </>
         </Layout>
     )
 }
